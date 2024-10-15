d1d9a636d6b8c5b5c3bb5f0897e72a1db2ece607
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-10-15 13:09:15 +0530

    feat: edit field types & add options

--- src/components/EditFormModal.tsx
+++ src/components/EditFormModal.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import Popup from "./Popup";
-import { MdDeleteOutline } from "react-icons/md";
+import React, { useState } from "react"
+import Popup from "./Popup"
+import { MdDeleteOutline, MdAdd } from "react-icons/md"
 
 interface IConfirmModal {
-  title: string;
-  subTitle?: string;
-  actionName: string;
-  onConfirm: () => void;
-  onCancel: () => void;
-  form: any;
+  title: string
+  subTitle?: string
+  actionName: string
+  onConfirm: () => void
+  onCancel: () => void
+  form: any
 }
 
 const EditFormModal = ({
@@ -17,52 +17,64 @@ const EditFormModal = ({
   onConfirm,
   onCancel,
 }: IConfirmModal) => {
-  const [formTitle, setFormTitle] = useState(form.title || "");
+  const [formTitle, setFormTitle] = useState(form.title || "")
   const [fields, setFields] = useState(
-    form.fields || [{ label: "" }] // Ensure at least one field exists
-  );
+    form.fields || [{ label: "", type: "text", options: [] }]
+  )
 
-  // Toast Message States
   const [messagePopupType, setMessagePopupType] = useState<
     "success" | "error" | "warning"
-  >("success");
-  const [popupMessage, setPopupMessage] = useState("");
-  const [showMessagePopup, setShowMessagePopup] = useState(false);
+  >("success")
+  const [popupMessage, setPopupMessage] = useState("")
+  const [showMessagePopup, setShowMessagePopup] = useState(false)
 
   const closePopup = () => {
-    setShowMessagePopup(false);
-  };
+    setShowMessagePopup(false)
+  }
 
-  // Handle form title change
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormTitle(e.target.value);
-  };
+    setFormTitle(e.target.value)
+  }
 
-  // Handle field label change
-  const handleFieldChange = (index: number, value: string) => {
-    const updatedFields = [...fields];
-    updatedFields[index].label = value;
-    setFields(updatedFields);
-  };
+  const handleFieldChange = (index: number, key: string, value: string) => {
+    const updatedFields = [...fields]
+    updatedFields[index][key] = value
+    setFields(updatedFields)
+  }
 
-  //TODO: Add new field
-  // const handleAddField = () => {
-  //   setFields([...fields, { label: "" }]); // Add a new empty field
-  // };
-
-  // Remove a field
   const handleRemoveField = (index: number) => {
-    const updatedFields = fields.filter((_: any, i: number) => i !== index);
-    setFields(updatedFields);
-  };
+    const updatedFields = fields.filter((_: any, i: number) => i !== index)
+    setFields(updatedFields)
+  }
+
+  const handleAddOption = (fieldIndex: number) => {
+    const updatedFields = [...fields]
+    updatedFields[fieldIndex].options.push("")
+    setFields(updatedFields)
+  }
+
+  const handleOptionChange = (
+    fieldIndex: number,
+    optionIndex: number,
+    value: string
+  ) => {
+    const updatedFields = [...fields]
+    updatedFields[fieldIndex].options[optionIndex] = value
+    setFields(updatedFields)
+  }
+
+  const handleRemoveOption = (fieldIndex: number, optionIndex: number) => {
+    const updatedFields = [...fields]
+    updatedFields[fieldIndex].options.splice(optionIndex, 1)
+    setFields(updatedFields)
+  }
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  
-    // Log the form data before sending
-    console.log('Submitting form with title:', formTitle);
-    console.log('Submitting form fields:', fields);
-  
+    e.preventDefault()
+
+    console.log("Submitting form with title:", formTitle)
+    console.log("Submitting form fields:", fields)
+
     try {
       const response = await fetch(`/api/forms/${form._id}`, {
         method: "PUT",
@@ -70,24 +82,28 @@ const EditFormModal = ({
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ title: formTitle, fields }),
-      });
-  
+      })
+
       if (response.ok) {
-        const result = await response.json();
-        setShowMessagePopup(true);
-        setPopupMessage("Form updated successfully!");
-        setMessagePopupType("success");
-        onConfirm(); // Close the modal or refresh data
+        const result = await response.json()
+        setShowMessagePopup(true)
+        setPopupMessage("Form updated successfully!")
+        setMessagePopupType("success")
+        onConfirm()
       } else {
-        const errorData = await response.json();
-        console.error("Failed to update form:", errorData.error);
-        alert("Failed to update form.");
+        const errorData = await response.json()
+        console.error("Failed to update form:", errorData.error)
+        setShowMessagePopup(true)
+        setPopupMessage("Failed to update form.")
+        setMessagePopupType("error")
       }
     } catch (error) {
-      console.error("Error updating form:", error);
-      alert("Error updating form.");
+      console.error("Error updating form:", error)
+      setShowMessagePopup(true)
+      setPopupMessage("Error updating form.")
+      setMessagePopupType("error")
     }
-  };
+  }
 
   return (
     <>
@@ -102,7 +118,7 @@ const EditFormModal = ({
         onClick={onCancel}
       >
         <div
-          className="rounded-lg bg-gray-900 p-8 shadow-2xl w-[400px]"
+          className="rounded-lg bg-gray-900 p-8 shadow-2xl w-[400px] sm:w-[600px] max-h-[80vh] overflow-y-auto editModalClass"
           onClick={(e) => e.stopPropagation()}
         >
           <h2 className="text-xl text-center text-blue-300">Edit Form:</h2>
@@ -127,33 +143,88 @@ const EditFormModal = ({
               <span>Form Fields:</span>
               <div className="flex flex-col">
                 {fields.map((field: any, index: number) => (
-                  <div className="flex items-center" key={index}>
-                    <span className="p-4 bg-blue-300 text-blue-900 text-sm mt-1 mb-2 rounded-lg mr-1">
-                      {index + 1}
-                    </span>
-                    <input
-                      type="text"
-                      value={field.label}
-                      className="w-full rounded-lg bg-gray-800 border-gray-200 p-4 text-sm shadow-sm mt-1 mb-2"
-                      onChange={(e) => handleFieldChange(index, e.target.value)}
-                      required
-                    />
-                    <button
-                      type="button"
-                      className="ml-2 text-white bg-red-500 p-3 rounded-lg mt-1 mb-2"
-                      onClick={() => handleRemoveField(index)}
-                    >
-                      <MdDeleteOutline className="text-xl" />
-                    </button>
+                  <div className="flex flex-col mb-4" key={index}>
+                    <div className="flex items-center">
+                      <span className="p-4 mr-2 bg-blue-300 text-blue-900 text-sm mt-1 mb-2 rounded-lg">
+                        {index + 1}
+                      </span>
+                      <div className="flex flex-col w-full">
+                        <input
+                          type="text"
+                          value={field.label}
+                          className="rounded-lg bg-gray-800 border-gray-200 p-3 text-sm shadow-sm mt-1 mb-2"
+                          onChange={(e) =>
+                            handleFieldChange(index, "label", e.target.value)
+                          }
+                          required
+                        />
+                        <select
+                          value={field.type}
+                          onChange={(e) =>
+                            handleFieldChange(index, "type", e.target.value)
+                          }
+                          className="rounded-lg bg-gray-800 border-gray-200 p-3 text-sm shadow-sm mt-1 mb-2"
+                        >
+                          <option value="text">Text</option>
+                          <option value="number">Number</option>
+                          <option value="email">Email</option>
+                          <option value="mcq">Multiple Choice</option>
+                          <option value="dropdown">Dropdown</option>
+                        </select>
+                      </div>
+                      <button
+                        type="button"
+                        className="ml-2 text-white bg-red-500 p-3 rounded-lg mt-1 mb-2"
+                        onClick={() => handleRemoveField(index)}
+                      >
+                        <MdDeleteOutline className="text-xl" />
+                      </button>
+                    </div>
+                    {(field.type === "mcq" || field.type === "dropdown") && (
+                      <div className="ml-8 mt-2">
+                        <span className="text-sm font-semibold">Options:</span>
+                        {field.options.map(
+                          (option: string, optionIndex: number) => (
+                            <div
+                              className="flex items-center mt-1"
+                              key={optionIndex}
+                            >
+                              <input
+                                type="text"
+                                value={option}
+                                className="w-full rounded-lg bg-gray-800 border-gray-200 p-2 text-sm shadow-sm"
+                                onChange={(e) =>
+                                  handleOptionChange(
+                                    index,
+                                    optionIndex,
+                                    e.target.value
+                                  )
+                                }
+                                required
+                              />
+                              <button
+                                type="button"
+                                className="ml-2 text-white bg-red-500 p-2 rounded-lg"
+                                onClick={() =>
+                                  handleRemoveOption(index, optionIndex)
+                                }
+                              >
+                                <MdDeleteOutline className="text-lg" />
+                              </button>
+                            </div>
+                          )
+                        )}
+                        <button
+                          type="button"
+                          className="mt-2 bg-green-600 text-white py-1 px-2 rounded-lg text-sm flex items-center"
+                          onClick={() => handleAddOption(index)}
+                        >
+                          <MdAdd className="mr-1" /> Add Option
+                        </button>
+                      </div>
+                    )}
                   </div>
                 ))}
-                {/* <button
-                  type="button"
-                  className="mt-2 bg-green-600 text-white py-2 px-4 rounded-lg"
-                  onClick={handleAddField}
-                >
-                  Add Field
-                </button> */}
               </div>
             </div>
 
@@ -167,7 +238,7 @@ const EditFormModal = ({
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default EditFormModal;
+export default EditFormModal
